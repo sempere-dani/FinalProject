509fd5fb1ad3ee14953cba25aad7f5be99e23186
Daniel Sempere <sempere.dani@gmail.com>
2014-05-24 10:26:04 +0200

    Creacion de los primeros metodos

--- FinalProject/src/GastosMes.java
+++ FinalProject/src/GastosMes.java
@@ -1,4 +1,26 @@
+import java.io.Serializable;
 
-public class GastosMes {
 
+public class GastosMes implements Serializable {
+	
+	//atributos GastoMes
+	private String nombreMes;
+	private int comida;
+	private int gasolina;
+	private int ocio;
+	private int servicios;
+	private int otros;
+	
+	public GastosMes(String month,int food,int gas,int freeTime,int serv,int others){
+		
+		month="enero";
+		food=100;
+		gas=50;
+		freeTime=150;
+		serv=175;
+		others=40;
+		
+	}
+	
+	
 }
